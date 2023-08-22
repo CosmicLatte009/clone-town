6fbcdd17d48b1aec10b7570af4ced126d9d80ba8
kyounghye <kyoung00828@gmail.com>
2023-08-22 20:11:27 +0900

    Rename: portfolioPage json파일 구조 변경

--- src/portfolioPage/PortfolioPage.jsx
+++ src/portfolioPage/PortfolioPage.jsx
@@ -1,11 +1,41 @@
-import portfolioData from './portfolio.json';
+import { useState } from 'react';
+
 import styles from './portfolioPage.module.scss';
 import PortfolioCard from './component/PortfolioCard';
 
 const PortfolioList = () => {
+  const [category, setCategory] = useState('All');
+  const [data, setData] = useState(portfolioData.data);
+
+  console.log('data', data);
+  console.log('data.category', data[0].category);
+
+  const clickTabHandler = () => {};
+
+  //첫 로딩시(카테고리가 All일 경우) 모든 카테고리를 보여줌
+  //이후 카테고리가 바뀔 때 마다
+  //map을 돌면서 각 인덱스의 카테고리 확인
+  //해당하는 카테고리의 detail을 새 배열에 저장
+  //detail만 저장된 새 배열을 data에 다시 세팅
+
   return (
     <>
-      <PortfolioCard portfolioData={portfolioData} />
+      <div className={styles.portfolio_tap}>
+        <ul>
+          <li>ALL</li>
+          <li>WEB</li>
+          <li>MALL</li>
+          <li>APP</li>
+          <li>IoT</li>
+          <li>Etc</li>
+        </ul>
+      </div>
+
+      <div className={styles.portfolio_list}>
+        {data.map((data, index) => {
+          return <PortfolioCard portfolioData={data} index={index} />;
+        })}
+      </div>
     </>
   );
 };
--- src/portfolioPage/component/PortfolioCard.jsx
+++ src/portfolioPage/component/PortfolioCard.jsx
@@ -1,48 +1,64 @@
 import styles from './PortfolioCard.module.scss';
+import Button from '../../components/Button';
 
-const PortfolioCard = ({ portfolioData }) => {
-  const data = portfolioData.data[0].detail;
-  console.log(process.env.PUBLIC_URL + data.img);
-
+const PortfolioCard = ({ portfolioData, index }) => {
+  const data = portfolioData;
   return (
     <>
       <div className={styles.container}>
-        <div className={styles.company_imgWrapper}>
-          <img src={process.env.PUBLIC_URL + data.img} />
-        </div>
-        <div className={styles.company_content}>
-          <ul>
-            <li>
-              <p>{data.Summary}</p>
-            </li>
-            <li>
-              <h5>{data.name}</h5>
-            </li>
-            <li>
-              <div className={styles.company_colorBox}>
-                <p>COLOR</p>
-                <div className={styles.company_colorPalette}>
-                  {data.color.map((color, index) => {
-                    return (
-                      <span
-                        key={'color' + index}
-                        style={{ backgroundColor: `${color}` }}
-                      ></span>
-                    );
-                  })}
+        <div className={styles.portWarpper}>
+          <div className={index % 2 == 0 ? styles.port1 : styles.port2}>
+            <div className={styles.company_imgWrapper}>
+              <div className={styles.company_Img}>
+                <img src={process.env.PUBLIC_URL + data.img} />
+              </div>
+            </div>
+            <div className={styles.company_contentWrapper}>
+              <ul>
+                <li>
+                  <p>{data.Summary}</p>
+                </li>
+                <li>
+                  <h5>{data.name}</h5>
+                </li>
+                <li>
+                  <div className={styles.company_colorBox}>
+                    <p>COLOR</p>
+                    <div className={styles.company_colorPalette}>
+                      {data.color.map((color, index) => {
+                        return (
+                          <span
+                            key={'color' + index}
+                            style={{ backgroundColor: `${color}` }}
+                          ></span>
+                        );
+                      })}
+                    </div>
+                  </div>
+                </li>
+                <li>
+                  <div className={styles.company_urlBtn}>
+                    <Button
+                      size="small"
+                      onClick={(e) => {
+                        window.location.href = data.url;
+                      }}
+                    >
+                      URL
+                    </Button>
+                  </div>
+                </li>
+              </ul>
+              <div className={styles.company_tagWrapper}>
+                <div className={styles.company_tag}>
+                  <ul>
+                    {data.tag.map((item, index) => {
+                      return <li key={'tag' + index}>#{item}</li>;
+                    })}
+                  </ul>
                 </div>
               </div>
-            </li>
-            <li>
-              <div className={styles.company_urlBtn}></div>
-            </li>
-          </ul>
-          <div className={styles.company_tag}>
-            <ul>
-              {data.tag.map((item, index) => {
-                return <li key={'tag' + index}>#{item}</li>;
-              })}
-            </ul>
+            </div>
           </div>
         </div>
       </div>
