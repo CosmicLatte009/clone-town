78e4501e3860646e88727b7487c0e9968c3a6d4e
Chaewon <lcw3714@naver.com>
2023-08-23 00:28:37 +0900

    Feat: 서비스 페이지 서비스 섹션 추가

---
+++ src/page/service/components/service-section/ServiceSection.js
@@ -0,0 +1,14 @@
+import Scroll from '../../../../components/Scroll';
+import styles from './ServiceSection.module.scss';
+
+const ServiceSection = () => {
+  return (
+    <section className={styles.ServiceSection}>
+      <h1 className={styles.Title}>RC SERVICE</h1>
+      <span className={styles.SubTitle}>서비스소개</span>
+      <Scroll />
+    </section>
+  );
+};
+
+export default ServiceSection;
