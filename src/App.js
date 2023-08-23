d1d42edd020dfe4f282c8bae5a0e4bd1ef28a92b
CosmicLatte009 <ohhm009@gmail.com>
2023-08-23 15:15:19 +0900

    Feat: 리액트 라우터 설치와 홈페이지 더미 컴포넌트 제작

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import HomePage from './page/HomePage';
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
