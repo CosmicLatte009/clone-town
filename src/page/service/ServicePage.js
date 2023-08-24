7b92fd32f71de54b3bd07d132cf977590e2ae8cf
Chaewon <lcw3714@naver.com>
2023-08-24 11:58:08 +0900

    Feat: 서비스 페이지 웹 섹션 추가

--- src/page/service/ServicePage.js
+++ src/page/service/ServicePage.js
@@ -1,10 +1,12 @@
 import styles from './ServicePage.module.scss';
 import ServiceSection from './components/service-section/ServiceSection';
+import WebSection from './components/web-section/WebSection';
 
 const ServicePage = () => {
   return (
     <section className={styles.ServiceSection}>
       <ServiceSection />
+      <WebSection />
     </section>
   );
 };
