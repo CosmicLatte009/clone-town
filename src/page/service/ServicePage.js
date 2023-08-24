f735d50ea08bcec7aa046cf0ed195a4040770fa9
Chaewon Lee <85218080+mmnmm8@users.noreply.github.com>
2023-08-24 12:14:08 +0900

    Feat: 서비스 페이지 유지보수 섹션 추가 (#13)
    Co-authored-by: Chaewon <lcw3714@naver.com>

--- src/page/service/ServicePage.js
+++ src/page/service/ServicePage.js
@@ -2,6 +2,7 @@ import styles from './ServicePage.module.scss';
 import ServiceSection from './components/service-section/ServiceSection';
 import WebSection from './components/web-section/WebSection';
 import AppSection from './components/app-section/AppSection';
+import MaintenanceSection from './components/mtn-section/MaintenanceSection';
 
 const ServicePage = () => {
   return (
@@ -9,6 +10,7 @@ const ServicePage = () => {
       <ServiceSection />
       <WebSection />
       <AppSection />
+      <MaintenanceSection />
     </section>
   );
 };
