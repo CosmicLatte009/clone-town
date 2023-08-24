7e8896b530a796a4f40b744d95559e26a11a0673
Chaewon <lcw3714@naver.com>
2023-08-24 12:05:20 +0900

    Feat: 서비스 페이지 앱 섹션 추가

--- src/page/service/ServicePage.js
+++ src/page/service/ServicePage.js
@@ -1,12 +1,14 @@
 import styles from './ServicePage.module.scss';
 import ServiceSection from './components/service-section/ServiceSection';
 import WebSection from './components/web-section/WebSection';
+import AppSection from './components/app-section/AppSection';
 
 const ServicePage = () => {
   return (
     <section className={styles.ServiceSection}>
       <ServiceSection />
       <WebSection />
+      <AppSection />
     </section>
   );
 };
