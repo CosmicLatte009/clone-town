e78cc5c885e14d3772fa0097a7e0ca22cae2d94a
CosmicLatte009 <ohhm009@gmail.com>
2023-08-24 13:45:13 +0900

    Feat: story 페이지에 공통적으로 필요한 이미지들 추가 및 story 라우터 설정

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomePage from './page/HomePage';
+import StoryNoticePage from './page/story/story-notice/StoryNoticePage';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/story-notice" element={<StoryNoticePage />} />
       </Routes>
     </BrowserRouter>
   );
