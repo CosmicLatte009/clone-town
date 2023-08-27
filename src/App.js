f08f8f4c6cc1d20e49a8ae4330e8fa6d15635b4e
Chaewon <lcw3714@naver.com>
2023-08-28 00:52:33 +0900

    Feat: 고정 버튼 추가

--- src/App.js
+++ src/App.js
@@ -3,11 +3,15 @@ import HomePage from './page/HomePage';
 import AboutPage from './page/about/AboutPage';
 import ServicePage from './page/service/ServicePage';
 import Nav from './page/nav/Nav';
+import FixedButton from './components/FixedButton';
+import MobileFixedButton from './components/MobileFixedButton';
 
 function App() {
   return (
     <BrowserRouter>
       <Nav />
+      <FixedButton />
+      <MobileFixedButton />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/about" element={<AboutPage />} />
