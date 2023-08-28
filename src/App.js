330e942a5832f25c717abd49c67eeb1be1058043
kyounghye <kyoung00828@gmail.com>
2023-08-28 13:59:12 +0900

    Fix: 메인페이지 섹션3 이미지 깨짐 오류 해결

--- src/App.js
+++ src/App.js
@@ -13,23 +13,27 @@ import StoryNoticePage from './page/story/story-notice/StoryNoticePage';
 import StoryLetterPage from './page/story/story-letter/StoryLetterPage';
 import StoryStudyPage from './page/story/story-study/StoryStudyPage';
 
+import Section3 from './page/main/Section3/Section3';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Nav />
-      <FixedButton />
-      <MobileFixedButton />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/about" element={<AboutPage />} />
-        <Route path="/service" element={<ServicePage />} />
-        <Route path="/portfolio" element={<PortfolioPage />} />
-        <Route path="/story-notice" element={<StoryNoticePage />} />
-        <Route path="/story-letter" element={<StoryLetterPage />} />
-        <Route path="/story-study" element={<StoryStudyPage />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <Section3 />
+      <BrowserRouter>
+        <Nav />
+        <FixedButton />
+        <MobileFixedButton />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/service" element={<ServicePage />} />
+          <Route path="/portfolio" element={<PortfolioPage />} />
+          <Route path="/story-notice" element={<StoryNoticePage />} />
+          <Route path="/story-letter" element={<StoryLetterPage />} />
+          <Route path="/story-study" element={<StoryStudyPage />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
