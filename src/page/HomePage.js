20c758f38272262352cd3396cc6fcc1b04c3c2c4
Chaewon <lcw3714@naver.com>
2023-08-29 11:38:00 +0900

    Fix: HomePage 메인페이지 컴포넌트 추가

--- src/page/HomePage.js
+++ src/page/HomePage.js
@@ -1,7 +1,21 @@
-// import MainBannerSection from './main/components/MainBannerSection';
+import MainBannerSection from './main/Section1/MainBannerSection';
+import Section2 from './main/Section2/Section2';
+import Section3 from './main/Section3/Section3';
+import Section4 from './main/Section4/Section4';
+import MainStorySection from './main/Section5/MainStorySection';
+import Footer from '../components/Footer';
 
 const HomePage = () => {
-  return <>{/* <MainBannerSection /> */}</>;
+  return (
+    <section>
+      <MainBannerSection />
+      <Section2 />
+      <Section3 />
+      <Section4 />
+      <MainStorySection />
+      <Footer />
+    </section>
+  );
 };
 
 export default HomePage;
